a7325bffde993360a602b996c4add0a180b7eec6
AaronDep <aaron@dealereprocess.com>
2018-01-03 08:59:18 -0600

    added Topic card loop and Topic basic data

--- src/components/HomeBody.jsx
+++ src/components/HomeBody.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import '../css/styles.css';
+import TopicCard from './TopicCard';
+
+
 
 class HomeBody extends Component {
+	constructor(props) {
+		super(props);
+		this.topics = this.props.topics;
+	}
   render() {
     return (
-        <div>
-          <h1>This is Forum Body</h1>
+        <div className="page" style={ {height: "80vh" } }>
+          <div className="content">
+          		<div className="container" style={{ maxWidth:'1050px', margin:'auto'}}>
+          				{this.topics.map((topic) => {
+          					return (
+          						<TopicCard key={topic.title} topic={topic}/>
+          					)
+          				})}
+          		</div>
+          </div>
         </div>
     );
   }
