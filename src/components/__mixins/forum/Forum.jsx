f0e9916ac40c734403237fad2b315caac4135650
AaronDep <aaron@dealereprocess.com>
2018-01-15 15:07:28 -0600

    topic row updates

--- src/components/__mixins/forum/Forum.jsx
+++ src/components/__mixins/forum/Forum.jsx
@@ -5,22 +5,48 @@ import TopicRow from '../topicrow/TopicRow';
 import './css/forum-styles.css';
 
 class Forum extends Component {
+	constructor(props) {
+		super(props);
+		this.expand = this.expand.bind();
+	}
+	expand(com) {
+		if (com.forumContent.clientHeight > 0) {
+			com.forumContent.style.height = 0 + 'px';
+			com.changeBtn('down');
+		} else {
+			com.forumContent.style.height = 'auto';
+			com.changeBtn();
+		}
+	}
+	changeBtn(position) {
+		if (position === 'down') {
+			this.arrow.style.transform = 'rotateZ(0deg) translateY(0)';
+		} else {
+			this.arrow.style.transform = 'rotateZ(180deg) translateY(20%)';
+		}
+	}
+	componentDidMount() {}
 	render() {
-		const {topics} = this.props;
+		const { topics } = this.props;
 		return (
 			<div className="forum">
-				<div className="forum-content">
-					<h2>Forum Component</h2>
-      				{ 
-      					topics.map((topic) => {
-									return (
-										<TopicRow key={topic.title} topic={topic}/>
-									)
-								}) 			
-      				}
+				<div className="forum-header">
+					<div className="forum-title">
+						<h2>Forum Component</h2>
+					</div>
+					<div ref={input => (this.arrow = input)} className="arrors" onClick={() => this.expand(this)}>
+						<div className="leftArrow" />
+						<div className="rightArrow" />
+					</div>
+				</div>
+				<div className="fl_c" />
+				<div ref={input => (this.forumContent = input)} className="forum-content">
+					{topics.map(topic => {
+						return <TopicRow key={Math.random()} topic={topic} />;
+					})}
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
--- src/components/__mixins/topicrow/TopicRow.jsx
+++ src/components/__mixins/topicrow/TopicRow.jsx
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import './css/topicrow.css'
+import './css/topicrow.css';
 import img1 from './img/forum_img.png';
 import img2 from './img/coolcat.png';
 import img3 from './img/freepik.jpg';
-
-
 
 class TopicRow extends Component {
 	constructor() {
@@ -12,7 +10,7 @@ class TopicRow extends Component {
 		this.categoryImg = this.categoryImg.bind();
 	}
 	categoryImg(category) {
-		switch(category) {
+		switch (category) {
 			case 'code':
 				return img2;
 			case 'other':
@@ -22,36 +20,43 @@ class TopicRow extends Component {
 		}
 	}
 	render() {
-
-		const { topic } = this.props; 
-		console.log(topic);
+		const { topic } = this.props;
+		//console.log(topic);
 		return (
 			<div className="topic">
 				<div className="row-left">
-					<img src={this.categoryImg(topic.category) } alt=""/>
+					<img src={this.categoryImg(topic.category)} alt="" />
 				</div>
 				<div className="row-middle">
 					<div className="topic-title">
-						<h3>{ topic.title }</h3>
+						<h3>{topic.title}</h3>
 					</div>
 					<div className="topic-description">
-						<p>{ topic.text }</p>
+						<p>{topic.text}</p>
 					</div>
 				</div>
 				<div className="row-right">
 					<div className="author-avatar">
-						<img src={topic.author_avatar} alt=""/>
+						<img src={topic.author_avatar} alt="" />
 					</div>
 					<div className="topic-meta">
-						<p>By: <span className="author">{ topic.author } </span></p>
-						<p>Status: <span className="author">Veteran</span></p>
-						<p>Posts: <span className="author">{ 10 } </span></p>
-						<p>Member Since: <span className="date">{ topic.date }</span></p>
+						<p>
+							By: <span className="author">{topic.author} </span>
+						</p>
+						<p>
+							Status: <span className="author">Veteran</span>
+						</p>
+						<p>
+							Posts: <span className="author">{10} </span>
+						</p>
+						<p>
+							Member Since: <span className="date">{topic.date}</span>
+						</p>
 					</div>
 				</div>
-				<div className="fl_c"></div>
+				<div className="fl_c" />
 			</div>
-		)
+		);
 	}
 }
 
