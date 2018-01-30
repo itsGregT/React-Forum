bdd4afbf5eb613f5b2885626fcb474edb5eecb01
Aaron <aronwebpro@gmail.com>
2018-01-29 20:24:46 -0600

    post.jsx state correction

--- src/components/pages/post/Post.jsx
+++ src/components/pages/post/Post.jsx
@@ -8,18 +8,29 @@ import categories from '../../../categories.json';
 class Post extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { post: {created: 'loading'}};
 		this.Id = this.props.params.postId;
+		const defaultState = { post: {} };
+		defaultState.post[this.Id] = {
+			topicId: '',
+			title: '',
+			authorAvatar: '',
+			authorId: '',
+			authorName: '',
+			category: '',
+			created: 'loading',
+			text: ''
+		};
+		this.state = defaultState;
 		this.title = this.props.params.title;
-	}	
+	}
 	componentWillMount() {
 		this.ref = base.bindToState('topics', {
 			context: this,
 			state: 'post',
 			queries: {
 				orderByKey: 'topics',
-				equalTo: this.Id,
-				}
+				equalTo: this.Id
+			}
 		});
 	}
 	render() {
@@ -27,46 +38,46 @@ class Post extends Component {
 		const { topics } = this.props;
 		let createdDate = '';
 		let createdTime = '';
-		if (this.state.post.created !== 'loading' && post && post.hasOwnProperty('created') ) {
+		if (post.created !== 'loading' && post && post.hasOwnProperty('created')) {
 			let date = new Date(post.created);
-			createdDate = date.getMonth()+1 + '/' + date.getDate() + ' ' + date.getFullYear();
-			createdTime = date.getHours() + ':' + date.getMinutes();	
+			createdDate = date.getMonth() + 1 + '/' + date.getDate() + ' ' + date.getFullYear();
+			createdTime = date.getHours() + ':' + date.getMinutes();
 		}
 		console.log(createdDate);
 		console.log(createdTime);
-		console.log(this.props);
+		console.log(this.state);
 		return (
 			<div>
-			<Header />
-			<div className="content" >
-	          	<div className="content" >
-				  	<div className="container">
-						<div className="left">
-							<SearchFilter categories={categories} /> 
-						</div>
-						<div className="right post-container">
-							<div className="post-title forum-header">
-								<h2>This is the post title.</h2>
+				<Header />
+				<div className="content">
+					<div className="content">
+						<div className="container">
+							<div className="left">
+								<SearchFilter categories={categories} />
 							</div>
-							<div className="full-post">
-								<div className="post">
-									<div className="post-info">Here is some post information.</div>
-									<p>This is the post.</p>
+							<div className="right post-container">
+								<div className="post-title forum-header">
+									<h2>This is the post title.</h2>
 								</div>
-								<div className="author-info">
-									<p>This is the author</p>
+								<div className="full-post">
+									<div className="post">
+										<div className="post-info">Here is some post information.</div>
+										<p>This is the post.</p>
+									</div>
+									<div className="author-info">
+										<p>This is the author</p>
+									</div>
+									<div className="fl_c" />
 								</div>
-								<div className="fl_c"></div>
 							</div>
+							<div className="fl_c" />
 						</div>
-						<div className="fl_c"></div>
 					</div>
 				</div>
+
+				<Footer />
 			</div>
-			
-			<Footer />
-			</div>
-		)
+		);
 	}
 }
 
